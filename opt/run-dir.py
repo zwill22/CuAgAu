22933c370c6ef6196078599d242a39db6d1234c5
Zack Williams <zack.williams@bristol.ac.uk>
2021-11-09 22:17:44 +0000

    run-dir.py

--- opt/run-dir.py
+++ opt/run-dir.py
@@ -2,7 +2,12 @@ import os
 import subprocess
 
 
-def get_header(system, queue, *args):
+def get_header(filename, **kwargs):
+    system = kwargs["system"]
+    queue = kwargs["queue"]
+
+    args = [kwargs[k] for k in ("time", "nodes", "ncpu", "mem", "path")]
+
     if system == 'pbs':
         output = """#!/bin/bash -l
 #PBS -N "{0}"
@@ -34,10 +39,14 @@ source ${{HOME}}/.bashrc
 cd {5}
 """
 
-    return output.format(*args)
+    return output.format(filename, *args)
 
 
-def get_host_specs(hostname, queue, ncpu):
+def get_host_specs(**data):
+    ncpu = data.get("ncpu")
+    hostname = data.get("hostname")
+    queue = data.get("queue")
+
     if ncpu < 0:
         raise ValueError("Number of cpus cannot be negative")
 
@@ -46,69 +55,72 @@ def get_host_specs(hostname, queue, ncpu):
         if ncpu > 16:
             raise ValueError("Too many cpus ({} > 16) for BC3".format(ncpu))
         if queue == "himem":
-            mem = 240
+            mem = 15
         else:
-            mem = 64
+            mem = 4
         workdir = True
     elif 'bc4' in hostname:
         system = 'slurm'
         if ncpu > 28:
             raise ValueError("Too many cpus ({} > 28) for BC4".format(ncpu))
         if queue == "himem":
-            mem = 480
+            mem = 18
         else:
-            mem = 120
+            mem = 5
         workdir = True
     elif 'bp1' in hostname:
         system = 'slurm'
         if queue == "amd":
-            mem = 950
+            mem = 7
             if ncpu > 120:
-                raise ValueError("Too many cpus for BP1 (amd): {} > 120".format(ncpu))
+                raise ValueError("Too many cpus BP1 (amd): {}".format(ncpu))
         elif queue == "highmem":
-            mem = 720
+            mem = 90
             if ncpu > 8:
-                raise ValueError("Too many cpus for BP1 (highmem): {} > 8".format(ncpu))
+                raise ValueError("Too many cpus BP1 (highmem): {}".format(ncpu))
         elif queue == "plong":
-            mem = 350
+            mem = 10
             if ncpu > 32:
-                raise ValueError("Too many cpus for BP1 (plong): {} > 32".format(ncpu))
+                raise ValueError("Too many cpus BP1 (plong): {}".format(ncpu))
         else:
-            mem = 150
+            mem = 5
             if ncpu > 32:
-                raise ValueError("Too many cpus for BP1: {} > 32".format(ncpu))
+                raise ValueError("Too many cpus BP1: {} > 32".format(ncpu))
         workdir = False
     else:
         raise ValueError("Unable to determine identity of host computer")
 
-    return system, mem, workdir
+    return system, mem * ncpu, workdir
  
 
-def generate_program_input(prog, system, *args, export=True):
-    if system == "slurm":
-        output = "\n\n"
-    elif system == "pbs":
+def generate_program_input(filename, single=True, **kwargs):
+    prog = kwargs["prog"]
+    ncpu = kwargs["ncpu"]
+
+    if single:
         output = "\n\n"
     else:
-        raise ValueError("Invalid queueing system: {}".format(system))
+        output = "\n\t"
     if prog == "qcore":
-        output += "qcore -n {3} \"{0}.in\" &> \"{0}.out\""
+        output += "qcore -n {1} \"{0}.in\" &> \"{0}.out\""
     elif prog == "orca":
         output += "$((which orca)) \"{0}.in\" &> \"{0}.out\""
     elif prog == "psi4":
-        if export:
+        if single:
             output += "export MKL_THREADING_LAYER=\"\"\n\n"
-        output += "psi4 -n {3} -i \"{0}.in\" -o \"{0}.out\""
+        output += "psi4 -n {1} -i \"{0}.in\" -o \"{0}.out\""
     else:
         raise ValueError("Unrecognised program: {}".format(prog)) 
-    output += "\n"
+    if single:
+        output += "\n"
 
-    return output.format(*args)
+    return output.format(filename, ncpu)
 
 
-def generate_file_output(filename, system, queue, workdir, prog, *args):
-    output = get_header(system, queue, filename, *args)
-    
+def generate_file_output(filename, **kwargs):
+    output = get_header(filename, **kwargs)
+
+    workdir = kwargs["workdir"]
     if not workdir:
         output += """
 
@@ -120,10 +132,11 @@ cp "{0}.xyz" $tmp
 cd $tmp
 """.format(filename)
     
-    output += generate_program_input(prog, system, filename, *args)
+    output += generate_program_input(filename, **kwargs)
 
+    path = kwargs["path"]
     if not workdir:
-        output += "\ncp {0}* {4}\ncd {4}\n".format(filename, *args)
+        output += "\ncp {0}* {1}\ncd {1}\n".format(filename, path)
 
     return output
 
@@ -133,36 +146,45 @@ def write_file(output, subfile):
         f.write(output) 
     
 
-def submit_file(subfile, system):
-    if queue == 'pbs':
+def submit_file(subfile, **kwargs):
+    system = kwargs["system"]
+
+    if system == 'pbs':
         subprocess.run(["qsub", subfile])
-    elif queue == 'slurm':
+    elif system == 'slurm':
         subprocess.run(["sbatch", subfile])
     else:
         raise ValueError("Invalid queue")
 
 
-def write_and_submit(output, filename, system, submit=True):
-    subfile = filename + '.sub'
+def write_and_submit(output, filename, submit=True, **kwargs):
+    path = kwargs["path"]
+    subfilepath = os.path.join(path, filename + '.sub')
 
-    write_file(output, subfile)
+    write_file(output, subfilepath)
     if submit:
-        submit_file(subfile, system)
+        submit_file(subfilepath, **kwargs)
     
 
-def file_submissions(system, *args, **kwargs):
-    for infile in os.listdir():
+def file_submissions(write=True, **kwargs):
+    path = kwargs["path"]
+    for infile in os.listdir(path):
         if not infile.endswith('.in'):
             continue
 
         filename = infile.split('.')[0]
 
-        if os.path.isfile(filename + ".out"):
+        outfile_path = os.path.join(path, filename + ".out")
+
+        if os.path.isfile(outfile_path):
             continue
 
-        output = generate_file_output(filename, system, *args)
+        output = generate_file_output(filename, **kwargs)
 
-        write_and_submit(output, filename, system, **kwargs)
+        if write:
+            write_and_submit(output, filename, **kwargs)
+        else:
+            print(output)
 
 
 def run_directory():
@@ -178,9 +200,13 @@ def run_directory():
     return False
 
 
-def generate_dir_output(filename, system, queue, workdir, prog, *args, path):
-    output = get_header(system, queue, filename, *args, path)
-    
+def generate_dir_output(filename, **kwargs):
+    output = get_header(filename, **kwargs)
+
+    workdir = kwargs["workdir"]
+    prog = kwargs["prog"]
+    path = kwargs["path"]
+
     if not workdir:
         output += """
 
@@ -197,10 +223,10 @@ cd $tmp
 for file in *.in; do
   if [ ! -f ${file%.in}.out ]]; then"""
 
-    output += generate_program_input(prog, system, "${file%.in}", *args)
+    output += generate_program_input("${file%.in}", **kwargs, single=False)
 
     if not workdir:
-        output += "cp ${{file%.in}}.* {}\ncd {}\n".format(path)
+        output += "cp ${{file%.in}}.* {0}\ncd {0}\n".format(path)
 
     output += """
   fi
@@ -210,70 +236,56 @@ done
     return output  
 
 
-def directory_submission(system, queue, *args, path, **kwargs):
+def directory_submission(write=True, **kwargs):
+    path = kwargs["path"]
+
     if not run_directory():
         return
 
     filename = os.path.split(path)[-1]
 
-    output = generate_dir_output(filename, system, queue, *args, path)
+    output = generate_dir_output(filename, **kwargs)
 
-    write_and_submit(output, filename, system, **kwargs)
+    if write:
+        write_and_submit(output, filename, **kwargs)
+    else:
+        print()
+        print(output)
 
 
 def main(*args):
-    if len(args) != 8:
-        raise ValueError("Invalid input arguments, length = {}".format(len(args)))
-    nnodes = int(args[0])
-    ncpu = int(args[1])
-    time = args[2]
-    wd = args[3]
-    files = args[4]
-    prog = args[5]
-    queue = args[6]
-    print_level = int(args[7]) 
- 
-    path = os.path.abspath(wd)
+    narg = len(args)
+    if len(args) != 7:
+        raise ValueError("Invalid number of input arguments ({})".format(narg))
+
+    names = ["nodes", "ncpu", "time", "wd", "files", "prog", "queue"]
+
+    data = dict(zip(names, args))
+    data["ncpu"] = int(data["ncpu"])
+    data["nodes"] = int(data["nodes"])
+
+    data["path"] = os.path.abspath(data["wd"])
 
     hostname = os.uname().nodename
-    system, mem, workdir = get_host_specs(hostname, queue, ncpu)
-    
-    kwargs = {"submit": False}
-    
-    if print_level > 0:
-        print("Hostname: {}".format(hostname))
-        print("Queueing system: {}".format(system))
-        print("Queue: {}".format(queue))
-        print("Program: {}".format(prog))
-        print("Filepath: {}".format(path))
-        if workdir:
-            print(" -- Using current dir as working dir")
-        else:
-            print(" -- Using tmp dir as working dir")
-        print()
-        print("Number of nodes: {}".format(nnodes))
-        print("Number of CPUs per node: {}".format(ncpu))
-        print("Time: {}".format(time))
+    data["hostname"] = hostname
+    data["system"], data["mem"], data["workdir"] = get_host_specs(**data)
+
+    files = data["files"]
+    nodes = data["nodes"]
 
     if files == "files":
-        if nnodes > 1:
+        if nodes > 1:
             raise ValueError("No MPI for file submission")
-        if print_level > 0:
-            print(" -- Submitting files individually")
-        file_submissions(system, queue, workdir, prog, time, nnodes, ncpu, mem, path, **kwargs)
+        file_submissions(**data)
     elif files == "all":
-        if print_level > 0:
-            print(" -- Submitting all files using single submission")
-        directory_submission(system, queue, workdir, prog, time, nnodes, ncpu, mem, path, **kwargs)
+        directory_submission(**data)
     else:
-        raise ValueError("Invalid file submmission method: {}".format(files))
+        raise ValueError("Invalid file submission method: {}".format(files))
  
 
 if __name__ == "__main__":
     import sys
 
-    args = sys.argv[1:]
+    argv = sys.argv[1:]
 
-    main(*args)
-
-  
+    main(*argv)
